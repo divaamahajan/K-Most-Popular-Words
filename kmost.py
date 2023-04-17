44868a29277a473dcf8ad50aec980c90cca76bee
Rushang Shah <rushshah@Rushangs-MacBook-Pro.local>
2023-04-17 12:49:29 -0700

    Added stop_words as .txt

--- kmost.py
+++ kmost.py
@@ -1,35 +1,10 @@
-import string
-import heapq
 import psutil
 import time
-import urllib.request
-import zipfile
 import os
-
-# set the path to the directory where zip files are stored
-dir_path = "./"
-zip_files = ['data_300MB.txt', 'data_2.5GB.txt', 'data_16GB.txt']
-
-# download the zip file
-url = 'https://drive.google.com/u/0/uc?id=1kaVM15rD9_O9HsvzrUKkIZ4R6ETUAdo4&export=download'
-zipname = 'data.zip'
-urllib.request.urlretrieve(url, zipname)
-
-
-# extract the files from the zip file
-with zipfile.ZipFile(zipname, 'r') as zip_ref:
-    zip_ref.extractall(dir_path)
 
 # set the number of top words to find
 k = int(input("Enter the number of top words to find: "))
 
-# set the chunk size for reading the file
-chunk_size = 1024 * 1024
-
-# load stop words
-stop_words_url = "https://gist.githubusercontent.com/sebleier/554280/raw/22976e2acb4b4ad7a91f7080fa698e4a2409a1d1/stopwords.txt"
-with urllib.request.urlopen(stop_words_url) as response:
-    stopwords = [word.decode('utf-8').strip() for word in response.readlines()]
 
 # loop through all zip files
 for filename in zip_files:
