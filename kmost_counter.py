a75dfb18f86238e5b12caad8e316549293ec8b37
Rushang Shah <rushshah@Rushangs-MacBook-Pro.local>
2023-04-20 08:49:58 -0700

    Create log function for counter algo. Will update the remaining ones

--- kmost_counter.py
+++ kmost_counter.py
@@ -2,6 +2,8 @@ import psutil
 import time
 from collections import Counter
 import re
+import datetime
+import os
 
 SIZE_5MB  = int(5  * 1024 * 1024 )# 5 MB
 SIZE_10MB = int(10 * 1024 * 1024 )# 10 MB
@@ -15,6 +17,16 @@ FILENAME_16GB = "data_16GB.txt"
 FILE_STOP_WORDS = "stop_words.txt"
 
 size_dict = {None: "None. Full file is being read at once" ,SIZE_5MB: '5MB', SIZE_10MB: '10MB', SIZE_20MB: '20MB', SIZE_40MB: '40MB'}
+word_results = ""
+
+def generate_logs(result):
+    global word_results
+    current_file = os.path.basename(__file__).split(".")[0]
+    direc = os.getcwd()
+    log_file = "log_" + current_file + ".txt"
+    with open(os.path.join(direc,log_file), 'a') as logs:
+        logs.write(result)
+        print(f"\nLogs appended")
 
 def read_stop_words(file_path):
     with open(file_path, "r", encoding="utf-8-sig") as sw:
@@ -46,16 +58,20 @@ def read_datafile(file_path, stop_words, chunk_size=None):
     return word_counts
 
 def print_top_words(word_counts, k):
+    global word_results
     # get the top k words from the Counter
     top_words = word_counts.most_common(k)
 
     # print the top k wordsprint("\nTop frequent words:")
-    print("Word".ljust(20) + "Count")
+    word_results += f"Word".ljust(21) + "Count"
+    # print("Word".ljust(20) + "Count")
     for word, count in top_words:
-        print("{:<20} {}".format(word, count))
+        # print("{:<20} {}".format(word, count))
+        word_results += "\n{:<20} {}".format(word, count)
 
 
-def print_statistics(start_time):
+def print_statistics(filename, start_time):
+    global word_results
     # calculate the running time
     end_time = time.time()
     running_time = end_time - start_time
@@ -64,14 +80,26 @@ def print_statistics(start_time):
     process = psutil.Process()
     memory_usage = process.memory_info().rss
     cpu_utilization = process.cpu_percent()
-    print(f"\n\nRunning time: {running_time:.2f} seconds")
-    print(f"\nMemory usage: {memory_usage / 1024 / 1024:.2f} MB")
-    print(f"\nCPU utilization: {cpu_utilization:.2f}%")
-    print("------\n")
+    # file_size = filename.split("_")[-1]
+    results = f"**************************************************************\
+                \nOutput logs\
+                \nFile name:\t{filename}\
+                \nDate:\t\t{datetime.datetime.now()}\
+                \n**************************************************************\n"
+    results += word_results
+    results += f"\n\n**************************************************************"
+    results += f"\n\nRunning time:\t\t{running_time:.2f} seconds\
+                \nMemory usage:\t\t{memory_usage / 1024 / 1024:.2f} MB\
+                \nCPU utilization:\t{cpu_utilization:.2f} %\n"
+    results += f'\n************************** END *******************************\n\n'
+    print(f"{results}")
+    generate_logs(results)
 
 
 def process_data(filename, stop_words,top_k, chunk_size=None, ):
-    print(f"\n\n******Chunk Size : {size_dict[chunk_size]} ********** \n")
+    global word_results
+    # print(f"\n\n******Chunk Size : {size_dict[chunk_size]} ********** \n")
+    word_results += f"\n\n**************** Chunk Size : {size_dict[chunk_size]} **********************\n\n"
     start_time = time.time()
 
     # count words from data file
@@ -81,7 +109,7 @@ def process_data(filename, stop_words,top_k, chunk_size=None, ):
     print_top_words(word_counts, top_k)
 
     # print the performance metrics
-    print_statistics(start_time)
+    print_statistics(filename,start_time)
 
 
 def main():
